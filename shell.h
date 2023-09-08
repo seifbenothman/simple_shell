e18bfa9e04ec248f069400cf9a8bd28c33d480c6
Ghassengaston0 <ghassengaston0@gmail.com>
2023-09-08 12:47:47 +0200

    g

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ *Display the shell prompt.
+ */
+void display_prompt(void);
+
+/**
+ *Read a command from the user.
+ * 
+ *@return A dynamically allocated string containing the user's input.
+ *         NULL is returned on EOF (Ctrl+D).
+ */
+char *read_command(void);
+
+#endif /* _SHELL_H_ */
