65e003b6e5657a17a4c5d28741e19c3bd8c2cdc3
Ghassengaston0 <ghassengaston0@gmail.com>
2023-09-09 12:08:02 +0200

    g

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "shell.h"
+
 int main(void) {
 	char *input = NULL;
 	size_t bufsize = 0;
