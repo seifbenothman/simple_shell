fa7810be70bed77913381c5ca3937375f840d872
seifbenothman <seifbenothman95@gmail.com>
2023-09-19 08:28:15 +0000

    0

--- shell1.c
+++ shell1.c
@@ -16,60 +16,65 @@ int main(void)
 	info_t info;
 	int exit_status;
 	pid_t pid;
+	char *args[2];
 
 	while (1)
 	{
-		display_prompt();
-
-		characters_read = getline(&input, &bufsize, stdin);
-
-		if (characters_read == -1) 
-		{
-			handle_error("Error reading input");
-			continue;
-		}
-		remove_newline(input);
-		printf("#cisfun$ ");
-		characters_read = getline(&input, &bufsize, stdin);
-
-		if (characters_read == -1)
-		{
-			printf("\n");
-			free(input);
-			exit(EXIT_SUCCESS);
-		}
-
-		if (input[characters_read - 1] == '\n')
-			input[characters_read - 1] = '\0';
-
-		if (strcmp(input, "exit") == 0)
-		{
-			exit_status = info.err_num;
-			if (exit_status == -2)
-			{
-				printf("Exit with status: %d\n", info.err_num);
-				free(input);
-				exit(info.err_num);
-			}
-			else if (exit_status == 1)
-				continue;
-		}
-		else if (strcmp(input, "cd") == 0)
-		{
-			_mycd(&info);
-			continue;
-		}
-
-		pid_t pid = fork();
+		pid = fork();
 
 		if (pid == -1)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
+
 		if (pid == 0)
 		{
-			char *args[] = {input, NULL};
+			display_prompt();
+
+			characters_read = getline(&input, &bufsize, stdin);
+
+			if (characters_read == -1)
+			{
+				handle_error("Error reading input");
+				continue;
+			}
+
+			remove_newline(input);
+			printf("#cisfun$ ");
+			characters_read = getline(&input, &bufsize, stdin);
+
+			if (characters_read == -1)
+			{
+				printf("\n");
+				free(input);
+				exit(EXIT_SUCCESS);
+			}
+
+			if (input[characters_read - 1] == '\n')
+				input[characters_read - 1] = '\0';
+
+			if (strcmp(input, "exit") == 0)
+			{
+				exit_status = info.err_num;
+				if (exit_status == -2)
+				{
+					printf("Exit with status: %d\n", info.err_num);
+					free(input);
+					exit(info.err_num);
+				}
+				else if (exit_status == 1)
+					continue;
+			}
+			else if (strcmp(input, "cd") == 0)
+			{
+				_mycd(&info);
+				continue;
+			}
+
+			args[0] = input;
+			args[1] = NULL;
+
 			if (execve(input, args, NULL) == -1)
 			{
 				perror("execve");
