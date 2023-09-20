e868ed18c7f8b75bbd67fa6ac5236040a638aed0
seifbenothman <seifbenothman95@gmail.com>
2023-09-20 23:13:58 +0000

    0

--- main.c
+++ main.c
@@ -6,119 +6,56 @@
 #include <sys/wait.h>
 #include "shell.h"
 
-char *find_command(const char *command) 
+#define
+
+/**
+ * execute_command - Execute a command entered by the user.
+ * @command: The command to execute.
+ * @env: Array of environment variables.
+ *
+ * This function should handle the execution logic for the shell.
+ * You can use functions like fork(), execvp(), and wait() to execute commands.
+ * For simplicity, let's just print the command for now.
+ */
+
+void execute_command(const char *command, char **env)
 {
-	char *path = getenv("PATH");
-	char *path_copy = strdup(path);
-
-	if (path_copy == NULL) 
-	{
-		perror("strdup");
-		exit(EXIT_FAILURE);
-	}
-
-	char *token = strtok(path_copy, ":");
-	while (token != NULL) 
-	{
-		char full_path[256];
-		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-
-		if (access(full_path, X_OK) == 0) 
-		{
-			free(path_copy);
-			return strdup(full_path);
-		}
-
-		token = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return (NULL);
+	printf("Executing: %s", command);
 }
 
-int main(void)
+/**
+ * main - Entry point of the simple shell program.
+ * @argc: Number of command-line arguments (unused).
+ * @argv: Array of command-line argument strings (unused).
+ * @env: Array of environment variables.
+ *
+ * This function initializes the shell and enters a loop to read and execute commands.
+ */
+int main(int argc, char **argv, char **env) 
 {
-	char *buffer = NULL;
-	size_t bufsize = 0;
-	ssize_t characters_read;
-	int status;
-	pid_t pid;
-	char *envp[] = {NULL};
+	char input[MAX_INPUT_LENGTH];
+	int line_number = 1;
 
-	while (1)
+	while (1) 
 	{
-		printf("#cisfun$ ");
-
-		characters_read = getline(&buffer, &bufsize, stdin);
-
-		if (characters_read == -1)
+		printf("$ ");
+		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
 		{
-			if (feof(stdin))
-			{
-				printf("\n");
-				free(buffer);
-				exit(EXIT_SUCCESS);
-			}
-			perror("getline");
-			free(buffer);
-			exit(EXIT_FAILURE);
+			break;
+		}
+		size_t input_length = strlen(input);
+		if (input_length > 0 && input[input_length - 1] == '\n') 
+		{
+			input[input_length - 1] = '\0';
 		}
 
-		buffer[strcspn(buffer, "\n")] = '\0';
-
-		pid = fork();
-
-		if (pid == -1)
+		if (strcmp(input, "exit") == 0) 
 		{
-			perror("fork");
-			free(buffer);
-			exit(EXIT_FAILURE);
+			break;
 		}
-
-		if (pid == 0)
-		{
-			char *command = strtok(buffer, " ");
-			char *full_path = find_command(command);
-
-			if (full_path == NULL)
-			{
-				fprintf(stderr, "./shell: %s: command not found\n", command);
-				free(buffer);
-				continue;
-			}
-
-			char **argv = (char **)malloc(2 * sizeof(char *));
-			if (argv == NULL)
-			{
-				perror("malloc");
-				free(buffer);
-				free(full_path);
-				exit(EXIT_FAILURE);
-			}
-
-			argv[0] = full_path;
-			argv[1] = NULL;
-
-			if (execve(full_path, argv, envp) == -1)
-			{
-				perror("execve");
-				free(buffer);
-				free(argv);
-				free(full_path);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			if (wait(&status) == -1)
-			{
-				perror("wait");
-				free(buffer);
-				exit(EXIT_FAILURE);
-			}
-		}
+		execute_command(input, env);
+		line_number++;
 	}
 
-	free(buffer);
-	return (EXIT_SUCCESS);
+	return 0;
 }
