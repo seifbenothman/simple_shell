8f55cf2e8d4566b29cbbd86963dec27154d0ef34
seifbenothman <seifbenothman95@gmail.com>
2023-09-22 00:31:42 +0000

    0

--- main.c
+++ main.c
@@ -3,29 +3,26 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include "shell.h"
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARG_SIZE 64
 
-
-char *getEnvVar(const char *envVarName, shell_data_t *shell_data) {
+char *getEnvVar(char *envVarName, char **env) {
 	int i = 0;
 	char *key;
 
-	while (shell_data->environment[i]) {
-		key = strtok(shell_data->environment[i], "=");
+	while (env[i]) {
+		key = strtok(env[i], "=");
 		if (strcmp(envVarName, key) == 0) {
 			return strtok(NULL, "\n");
 		}
 		i++;
 	}
-	return NULL;
+	return (NULL);
 }
 
-void findExecutable(const char *command, char *exePath, shell_data_t *shell_data) {
-	char *path = getEnvVar("PATH", shell_data);
+void findExecutable(char *command, char *exePath, char *pathVar) {
+	char *path = getenv(pathVar);
 	char *token;
 
 	token = strtok(path, ":");
@@ -57,4 +54,66 @@ void handleSpecialChars(char *arg) {
 			k++;
 		}
 	}
+}
+
+void executeCommand(char *inputCmd, char *pathVar, int *commandCount) {
+	char *args[MAX_ARG_SIZE];
+	int argCount = 0;
+	int j;
+
+	char *token = strtok(inputCmd, " \t\n");
+	pid_t pid;
+
+	while (token != NULL && argCount < MAX_ARG_SIZE - 1) {
+		args[argCount++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[argCount] = NULL;
+
+	for (j = 0; j < argCount; j++) {
+		handleSpecialChars(args[j]);
+	}
+
+	pid = fork();
+
+	if (pid == -1) {
+		perror("fork");
+	} else if (pid == 0) {
+		char exePath[50];
+
+		findExecutable(args[0], exePath, pathVar);
+		if (exePath[0] == '\0') {
+			fprintf(stderr, "hsh: %d: %s: command not found\n", (*commandCount)++, args[0]);
+			exit(1);
+		}
+		execvp(exePath, args);
+		perror("./myShell");
+
+		exit(1);
+	} else {
+		wait(NULL);
+	}
+}
+
+int main() {
+	char userInput[MAX_INPUT_SIZE];
+	int commandCount = 1; /* Initialize commandCount to 1 for each shell session */
+
+	while (1) {
+		printf("$ ");
+		if (fgets(userInput, sizeof(userInput), stdin) == NULL) {
+			break;
+		}
+
+		userInput[strcspn(userInput, "\n")] = '\0';
+
+		if (strcmp(userInput, "exit") == 0) {
+			break;
+		}
+
+		executeCommand(userInput, "PATH", &commandCount);
+		commandCount++;
+	}
+
+	return (0);
 }
